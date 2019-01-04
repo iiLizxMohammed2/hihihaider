3004fd031998b29c8bf7ce054e44218773a14352
iiLizxMohammed2 <42420781+iiLizxMohammed2@users.noreply.github.com>
2019-01-04 21:36:33 +0300

    Delete leave.js

--- musiccommands/leave.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, message, args, ops) => {
-    if (!message.member.voiceChannel) return message.channel.send('Please connect to voice channel.');
-    if (!message.guild.me.voiceChannel) return message.channel.send('_Sorry_ , the bot isn\'t  connected to the channel.');
-    if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send('_Sorry_ , you aren\'t connected to the same channle.');
-    message.guild.me.voiceChannel.leave();
-    message.channel.send('leaving Channle>>>>>')
-}
